4290d04fad08c9210a4886103739ac1cd57fa38d
pratik sharma <123287083+pratik2061@users.noreply.github.com>
2024-05-31 21:07:00 +0545

    ✔ Project completed

--- TODO/todo.js
+++ TODO/todo.js
@@ -1,16 +1,28 @@
 let TodoList = [];
+DisplayItem();
 
 function addTodo() {
     let InputElement = document.querySelector('#todo-input');
+    let DateElement = document.querySelector('#todo-date');
     let TodoItem = InputElement.value;
-    TodoList.push(TodoItem);
+    let TodoDate = DateElement.value; 
+    TodoList.push({Itemname : TodoItem , Duedate : TodoDate});
     InputElement.value = '';
+    DateElement.value = '';
      DisplayItem();
 }
 function DisplayItem(){
-    let TodoDisplay = document.querySelector("#TodoDisplay")
-    TodoDisplay.innerHTML = ''
-    for (let i = 0; i < TodoList.length; i++) {
-        TodoDisplay.innerHTML = TodoDisplay.innerHTML + "<br>" + TodoList[i];
+    let dipslayElement = document.querySelector('.displayElement');
+    let newHtml ='';
+    for(i=0; i<TodoList.length; i++){
+        // let item = TodoList[i].Itemname
+        // let date = TodoList[i].Duedate
+        let {Itemname , Duedate} = TodoList[i]
+            newHtml = newHtml +`
+            <span>${Itemname}</span>
+            <span>${Duedate}</span>
+            <button id="delete" onclick ="TodoList.splice(${i},1);DisplayItem()">Delete</button>
+            `
     }
+ dipslayElement.innerHTML = newHtml;
 }
